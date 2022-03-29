2b535bd1b641edceaf92cd56f55023d7ace62bf1
Douglas Taquary <douglastaquary@gmail.com>
2022-03-29 19:54:43 -0300

    Initial Commit

---
+++ CrossfitPR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrossfitPR
+//
+//  Created by Douglas Taquary on 29/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
