f78c3d9da4dcdc248b1f66a85bbedb384be3bc63
Douglas Taquary <douglastaquary@gmail.com>
2022-04-28 18:24:08 -0300

    Fix insights view

--- CrossfitPR/Insights/InsightsView.swift
+++ CrossfitPR/Insights/InsightsView.swift
@@ -11,9 +11,12 @@ import CoreData
 
 struct InsightsView: View {
     @State var biggestPoint: DataPoint = DataPoint.init(value: 0.0, label: "", legend: Legend(color: .green, label: "", order: 1))
-    @State var lowPoint: DataPoint = DataPoint.init(value: 0.0, label: "", legend: Legend(color: .gray, label: "", order: 2))
+    @State var evolutionPoint: DataPoint = DataPoint.init(value: 0.0, label: "", legend: Legend(color: .yellow, label: "", order: 2))
+    @State var lowPoint: DataPoint = DataPoint.init(value: 0.0, label: "", legend: Legend(color: .gray, label: "", order: 3))
     @State var limit: DataPoint = DataPoint(value: 0, label: "", legend: Legend(color: .clear, label: ""))
     @State var barPoints: [DataPoint] = []
+    @State var biggestPRName: String = ""
+    @State var biggestPR: PR?
     
     @EnvironmentObject var store: InsightsStore
     @FetchRequest(entity: PR.entity(), sortDescriptors: [], predicate: NSPredicate(format: "prName != %@", PRType.empty.rawValue))
@@ -22,7 +25,7 @@ struct InsightsView: View {
     var body: some View {
         Form {
             Section("Resume") {
-                HorizontalBarChartView(dataPoints: [biggestPoint, lowPoint])
+                HorizontalBarChartView(dataPoints: [biggestPoint, evolutionPoint, lowPoint])
             }
             Section("Graph Details") {
                 if barPoints.isEmpty {
@@ -32,8 +35,8 @@ struct InsightsView: View {
                 }
             }
             Section("PR informations") {
-                HSubtitleView(title: "biggest pr", subtitle: "160 lb")
-                HSubtitleView(title: "percentage pr", subtitle: "70 %")
+                HSubtitleView(title: "the biggest pr is " + "\(biggestPR?.prName ?? "")".lowercased(), subtitle: "\(biggestPR?.prValue ?? 0) lb")
+                HSubtitleView(title: "percentage pr", subtitle: "\(biggestPR?.percentage.clean ?? "") %")
             }
         }.onAppear {
             loadGraph()
@@ -59,11 +62,21 @@ struct InsightsView: View {
     private func loadPRInfos() {
         let max: Int = prs.map { $0.prValue }.max() ?? 0
         let min: Int = prs.map { $0.prValue }.min() ?? 0
+        let evolutionPRselected = prs.filter { pr in
+            return pr.prValue < max && pr.prValue > min && pr.prName != biggestPRName
+        }.sorted {
+            $0.prValue > $1.prValue
+        }.first
+
+        evolutionPoint = DataPoint.init(value: Double(evolutionPRselected?.prValue ?? 0), label: "\(evolutionPRselected?.prValue ?? 0) lb", legend: Legend(color: .yellow, label: "\(evolutionPRselected?.prName ?? "")", order: 2))
+        
         for pr in prs {
             if pr.prValue == max {
                 biggestPoint = DataPoint.init(value: Double(pr.prValue), label: "\(pr.prValue) lb", legend: Legend(color: .green, label: "\(pr.prName)", order: 1))
+                biggestPRName = pr.prName
+                biggestPR = pr
             } else if pr.prValue == min {
-                lowPoint = DataPoint.init(value: Double(pr.prValue), label: "\(pr.prValue) lb", legend: Legend(color: .gray, label: "\(pr.prName)", order: 2))
+                lowPoint = DataPoint.init(value: Double(pr.prValue), label: "\(pr.prValue) lb", legend: Legend(color: .gray, label: "\(pr.prName)", order: 3))
             }
         }
     }
