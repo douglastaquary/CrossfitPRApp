f26e624cf7ae83de1b209f97e75a8ba2161cbeb2
Douglas Taquary <douglastaquary@gmail.com>
2022-09-11 15:18:44 -0300

    Support localization on insight and new record screen. Clean unused files.

--- CrossfitPR/Onboarding/OnboardingView.swift
+++ CrossfitPR/Onboarding/OnboardingView.swift
@@ -17,20 +17,30 @@ struct OnboardingView: View {
                     .fontWeight(.heavy)
                     .font(.system(size: 36))
                     .frame(width: 300, alignment: .leading)
-            
-            HViewImageAndText(image: "figure.strengthtraining.traditional", imageColor: .green, title: "onboarding.item2.title", description: "onboarding.item2.description")
-            
+            HViewImageAndText(
+                image: "figure.strengthtraining.traditional",
+                imageColor: .green,
+                title: "onboarding.item2.title",
+                description: "onboarding.item2.description"
+            )
             HViewImageAndText(
                 image: "list.bullet",
                 imageColor: .green,
                 title: "onboarding.item1.title",
                 description: "onboarding.item1.description"
             )
-            
-            HViewImageAndText(image: "chart.xyaxis.line", imageColor: .green, title: "onboarding.item3.title", description: "onboarding.item3.description")
-            
-            HViewImageAndText(image: "chart.bar", imageColor: .green, title: "onboarding.item4.title", description: "onboarding.item4.description")
-  
+            HViewImageAndText(
+                image: "chart.xyaxis.line",
+                imageColor: .green,
+                title: "onboarding.item3.title",
+                description: "onboarding.item3.description"
+            )
+            HViewImageAndText(
+                image: "chart.bar",
+                imageColor: .green,
+                title: "onboarding.item4.title",
+                description: "onboarding.item4.description"
+            )
             Spacer()
             VStack {
                 Button("onboarding.button.title"){
@@ -47,41 +57,8 @@ struct OnboardingView: View {
     }
 }
 
-struct NewDetail: View {
-    var image: String
-    var imageColor: Color
-    var title: String
-    var description: String
-
-    var body: some View {
-        HStack(alignment: .center) {
-            HStack {
-                Image(systemName: image)
-                    .font(.system(size: 50))
-                    .frame(width: 50)
-                    .foregroundColor(imageColor)
-                    .padding()
-
-                VStack(alignment: .leading) {
-                    Text(title).bold()
-                
-                    Text(description)
-                        .fixedSize(horizontal: false, vertical: true)
-                }
-            }.frame(width: 340, height: 100)
-        }
-    }
-}
-
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
-    }
-}
-
-struct NewDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        NewDetail(image: "heart.fill", imageColor: .pink, title: "More Personalized", description: "Top Stories picked for you and recommendations from siri.")
-        .previewLayout(.sizeThatFits)
     }
 }
